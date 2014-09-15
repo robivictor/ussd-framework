c1ad3821f0180820d9b675df6c4307731d0b3aac
Samora Dake <samoradake@gmail.com>
2014-09-15 17:09:51 +0000

    UssdInput

---
+++ UssdFramework/UssdInput.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UssdFramework
+{
+    public class UssdInput
+    {
+        /// <summary>
+        /// Input's name.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Input's displayed name.
+        /// </summary>
+        public string DisplayName { get; set; }
+        /// <summary>
+        /// List of input's options.
+        /// </summary>
+        public List<UssdInputOptions> Options { get; set; }
+        /// <summary>
+        /// Check if input has options.
+        /// </summary>
+        public bool HasOptions { get { return Options.Count > 0; } }
+        
+        /// <summary>
+        /// Initialize with only input's name.
+        /// </summary>
+        /// <param name="name">Input's name.</param>
+        public UssdInput(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Initialize with input's name and displayed name.
+        /// </summary>
+        /// <param name="name">Input's name.</param>
+        /// <param name="displayName">Input's displayed name.</param>
+        public UssdInput(string name, string displayName) : this(name)
+        {
+            DisplayName = displayName;
+        }
+
+        /// <summary>
+        /// Initalize with input's name, displayed name and list of options.
+        /// </summary>
+        /// <param name="name">Input's name.</param>
+        /// <param name="displayName">Input's displayed name.</param>
+        /// <param name="options">List of input options.</param>
+        public UssdInput(string name, string displayName, List<UssdInputOptions> options) : this(name, displayName)
+        {
+            Options = options;
+        }
+
+        /// <summary>
+        /// Initialize with input's name and list of options.
+        /// </summary>
+        /// <param name="name">Input's name.</param>
+        /// <param name="options">List of input options.</param>
+        public UssdInput(string name, List<UssdInputOptions> options) : this(name)
+        {
+            Options = options;
+        }
+    }
+}
---
+++ UssdFramework/UssdInputOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UssdFramework
+{
+    public class UssdInputOptions
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
