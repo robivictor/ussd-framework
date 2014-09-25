16d1b4c9989fdb8cbf4faf3e0e41b67e5057959a
Samora Dake <samoradake@gmail.com>
2014-09-25 08:31:24 +0000

    Setup overloads and support for redis database numbers

--- UssdFramework/Setup.cs
+++ UssdFramework/Setup.cs
@@ -37,5 +37,42 @@ namespace UssdFramework
             Redis = ConnectionMultiplexer.Connect(redisAddress).GetDatabase();
             UssdScreens = screens;
         }
+
+        /// <summary>
+        /// Setup initializer
+        /// </summary>
+        /// <param name="name">App name</param>
+        /// <param name="redisAddress">Address/location of Redis instance. Uses StackExchange.Redis underneath.</param>
+        /// <param name="redisDatabaseNumber">Redis database number.</param>
+        /// <param name="screens">A dictionary containing <param name="screens">USSD Screens</param></param>
+        public Setup(string name, string redisAddress, int redisDatabaseNumber, Dictionary<string, UssdScreen> screens)
+        {
+            Name = name;
+            Redis = ConnectionMultiplexer.Connect(redisAddress).GetDatabase(redisDatabaseNumber);
+            UssdScreens = screens;
+        }
+
+        /// <summary>
+        /// Setup initializer
+        /// </summary>
+        /// <param name="name">App name</param>
+        /// <param name="screens">A dictionary containing <param name="screens">USSD Screens</param></param>
+        /// <param name="redisAddress">Address/location of Redis instance. Uses StackExchange.Redis underneath.</param>
+        public Setup(string name, Dictionary<string, UssdScreen> screens, string redisAddress) 
+            : this(name, redisAddress, screens)
+        {
+        }
+
+        /// <summary>
+        /// Setup initializer
+        /// </summary>
+        /// <param name="name">App name.</param>
+        /// <param name="screens">A dictionary containing <param name="screens">USSD Screens</param></param>
+        /// <param name="redisAddress">Address/location of Redis instance. Uses StackExchange.Redis underneath.</param>
+        /// <param name="redisDatabaseNumber">Redis database number.</param>
+        public Setup(string name, Dictionary<string, UssdScreen> screens, string redisAddress, int redisDatabaseNumber)
+            : this(name, redisAddress, redisDatabaseNumber, screens)
+        {
+        }
     }
 }
